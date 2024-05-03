544455d476cf4e67c272262162104ba4b163696f
archit.mehta <archit.mehta@pirimidtech.com>
2024-05-03 11:04:35 +0530

    - remove caching code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableJpaRepositories
+@EnableCaching
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/example/demo/service/cache/CacheService.java
+++ src/main/java/com/example/demo/service/cache/CacheService.java
@@ -1,67 +1,61 @@
 package com.example.demo.service.cache;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
+//@Service
 @Slf4j
 public class CacheService {
 
     private static final String ELECTRICITY_RATE_MAP_KEY_PREFIX = "electricity_rate_";
     private static final String CARBON_EMISSION_RATE_MAP_KEY_PREFIX = "carbon_emission_rate_";
 
-
-    private RedisTemplate<String, BigDecimal> electricityRateTemplate;
-
-
-
-    public CacheService(RedisTemplate<String, BigDecimal> electricityRateTemplate
-) {
-        this.electricityRateTemplate = electricityRateTemplate;
-    }
-
-    /**
-     * Save Electricity Rate to Redis Cache.
-     * 
-     * @param key   The key under which the electricity rate will be stored.
-     * @param value The electricity rate value to be stored.
-     */
-    public void saveElectricityRate(String key, BigDecimal value) {
-
-        String redisKey = ELECTRICITY_RATE_MAP_KEY_PREFIX + key;
-        log.info("Electricity Key : {}", redisKey);
-        electricityRateTemplate.opsForValue().set(redisKey, value);
-    }
-
-    /**
-     * Get Electricity Rate from Redis Cache.
-     * 
-     * @param key The key under which the electricity rate is stored.
-     * @return The electricity rate value.
-     */
-    public BigDecimal getElectricityRate(String key) {
-        return electricityRateTemplate.opsForValue().get(ELECTRICITY_RATE_MAP_KEY_PREFIX + key);
-    }
-
-
-    /**
-     * Get Electricity Rate Map from Redis Cache.
-     * 
-     * @return The electricity rate map.
-     */
-    public Map<String, BigDecimal> getElectricityRateMap() {
-        Map<String, BigDecimal> electricityRateMap = new HashMap<>();
-        for (String key : electricityRateTemplate.keys(ELECTRICITY_RATE_MAP_KEY_PREFIX + "*")) {
-            String actualKey = key.replace(ELECTRICITY_RATE_MAP_KEY_PREFIX, "");
-            electricityRateMap.put(actualKey, electricityRateTemplate.opsForValue().get(key));
-        }
-        return electricityRateMap;
-    }
+//
+//    private RedisTemplate<String, BigDecimal> electricityRateTemplate;
+//
+//
+//
+//    public CacheService(RedisTemplate<String, BigDecimal> electricityRateTemplate
+//) {
+//        this.electricityRateTemplate = electricityRateTemplate;
+//    }
+//
+//    /**
+//     * Save Electricity Rate to Redis Cache.
+//     *
+//     * @param key   The key under which the electricity rate will be stored.
+//     * @param value The electricity rate value to be stored.
+//     */
+//    public void saveElectricityRate(String key, BigDecimal value) {
+//
+//        String redisKey = ELECTRICITY_RATE_MAP_KEY_PREFIX + key;
+//        log.info("Electricity Key : {}", redisKey);
+//        electricityRateTemplate.opsForValue().set(redisKey, value);
+//    }
+//
+//    /**
+//     * Get Electricity Rate from Redis Cache.
+//     *
+//     * @param key The key under which the electricity rate is stored.
+//     * @return The electricity rate value.
+//     */
+//    public BigDecimal getElectricityRate(String key) {
+//        return electricityRateTemplate.opsForValue().get(ELECTRICITY_RATE_MAP_KEY_PREFIX + key);
+//    }
+//
+//
+//    /**
+//     * Get Electricity Rate Map from Redis Cache.
+//     *
+//     * @return The electricity rate map.
+//     */
+//    public Map<String, BigDecimal> getElectricityRateMap() {
+//        Map<String, BigDecimal> electricityRateMap = new HashMap<>();
+//        for (String key : electricityRateTemplate.keys(ELECTRICITY_RATE_MAP_KEY_PREFIX + "*")) {
+//            String actualKey = key.replace(ELECTRICITY_RATE_MAP_KEY_PREFIX, "");
+//            electricityRateMap.put(actualKey, electricityRateTemplate.opsForValue().get(key));
+//        }
+//        return electricityRateMap;
+//    }
 
 
 }
