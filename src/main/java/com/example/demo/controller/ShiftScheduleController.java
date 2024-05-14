3dd1699312826ee0c2eef73d978e0d8033b59933
Shivam <shivamprajapati035@gmail.com>
2024-05-14 11:56:23 +0530

    Added CRUD API for Shift-Schedule

---
+++ src/main/java/com/example/demo/controller/ShiftScheduleController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.pojos.request.ShiftScheduleRequest;
+import com.example.demo.pojos.response.ShiftScheduleResponse;
+import com.example.demo.service.ShiftScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/shift-schedule")
+public class ShiftScheduleController {
+    private final ShiftScheduleService shiftScheduleService;
+
+    @PostMapping("create")
+    @PreAuthorize("hasAnyAuthority('VENDOR','MANAGER')")
+    public ResponseEntity<ShiftScheduleResponse> createShiftScheduleSchedule(@RequestBody ShiftScheduleRequest req) {
+        return ResponseEntity.ok(shiftScheduleService.createShiftSchedule(req));
+    }
+
+    @GetMapping("get-by-id")
+    @PreAuthorize("hasAnyAuthority('VENDOR','MANAGER','TECHNICIAN')")
+    public ResponseEntity<ShiftScheduleResponse> getShiftScheduleScheduleById(@RequestParam UUID id) {
+        return ResponseEntity.ok(shiftScheduleService.getShiftScheduleById(id));
+    }
+
+    @PutMapping("update-by-id")
+    @PreAuthorize("hasAnyAuthority('VENDOR','MANAGER')")
+    public ResponseEntity<ShiftScheduleResponse> updateShiftScheduleScheduleById(@RequestParam UUID id,
+            @RequestBody ShiftScheduleRequest req) {
+        return ResponseEntity.ok(shiftScheduleService.updateShiftScheduleById(id, req));
+    }
+
+    @DeleteMapping("delete-by-id")
+    @PreAuthorize("hasAnyAuthority('VENDOR','MANAGER')")
+    public ResponseEntity<String> deleteShiftScheduleScheduleById(@RequestParam UUID id) {
+        return ResponseEntity.ok(shiftScheduleService.deleteShiftScheduleById(id));
+    }
+
+}
