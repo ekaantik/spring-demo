3c857d0579f0fd860a1fa78a87bd2c913d5120e4
Shivam <shivamprajapati035@gmail.com>
2024-05-19 01:09:54 +0530

    Image & Video APIs
    Fixed Image & Video API bugs.
    Added Utility Class & Cleaned up Code.
    Added Video Download API.
    Added Delete APIs.

--- src/main/java/com/example/demo/service/VideoService.java
+++ src/main/java/com/example/demo/service/VideoService.java
@@ -1,27 +1,29 @@
 package com.example.demo.service;
 
+import com.example.demo.Utils;
 import com.example.demo.constants.VideoCategories;
 import com.example.demo.entity.Videos;
 import com.example.demo.pojos.response.VideoUploadResponse;
 import com.example.demo.repository.VideoRepoService;
-import com.example.demo.security.utils.JwtTokenService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @Service
 @Slf4j
 public class VideoService {
@@ -36,76 +38,132 @@ public class VideoService {
     private String videoPath;
 
     private final VideoRepoService videoRepoService;
-    private final JwtTokenService jwtTokenService;
 
-    @Autowired
-    public VideoService(VideoRepoService videoRepoService , JwtTokenService jwtTokenService){
-        this.videoRepoService = videoRepoService;
-        this.jwtTokenService = jwtTokenService;
-    }
+    /**
+     * Processes and uploads a video file for a specific store and category.
+     *
+     * @param videoCategory The category of the video.
+     * @param file          The video file to be uploaded.
+     * @param storeId       The unique identifier of the store.
+     * @return VideoUploadResponse containing information about the uploaded video
+     *         if successful.
+     * @throws RuntimeException if an error occurs during the upload process.
+     */
+    public VideoUploadResponse processVideoForUpload(VideoCategories videoCategory, MultipartFile file, UUID storeId) {
 
-    public VideoUploadResponse processVideoForUpload(VideoCategories videoCategory, MultipartFile file, UUID storeId){
-        String dirPath =  new StringBuilder().append(videoPath).append(storeId)
-                .append("/videos/").append(videoCategory.name()).append("/").toString();
         try {
-            String filePath = new StringBuilder(dirPath).append(file.getOriginalFilename()).toString();
 
-            if (! Objects.isNull(videoRepoService.findByPath(filePath))){
-                String updatedFileName = addSuffixToFileName(file.getOriginalFilename());
-                filePath = new StringBuilder(dirPath).append(updatedFileName).toString();
+            // Original Path
+            String originalPath = Utils.buildDirectoryPath(videoPath,
+                    storeId,
+                    "videos",
+                    videoCategory.toString(),
+                    file.getOriginalFilename());
+
+            String filePath = originalPath;
+
+            // Create Unique Path
+            int counter = 1;
+            while (!Objects.isNull(videoRepoService.findByPath(filePath))) {
+                filePath = Utils.addSuffixToFilePath(originalPath, counter);
+                counter++;
             }
-            log.info("file path : {} dire path : {} " , filePath, dirPath);
-            createUploadDirectoryIfNotExists(dirPath);
-            file.transferTo(new File(filePath));
-            Videos savedVideo = videoRepoService.save(filePath,storeId,videoCategory);
+
+            // Create Directory
+            Utils.createDirectory(filePath);
+
+            // Save File
+            File videoFile = new File(filePath);
+            file.transferTo(videoFile);
+
+            // Save Video
+            Videos savedVideo = videoRepoService.save(filePath, storeId, videoCategory);
+
+            // Build Response
             VideoUploadResponse response = VideoUploadResponse.builder()
                     .videoId(savedVideo.getId())
                     .storeId(savedVideo.getStore().getId())
                     .category(savedVideo.getCategory())
                     .message("Successfully Uploaded File")
                     .build();
+
+            log.info("Video uploaded successfully with id : {}", savedVideo.getId());
             return response;
         } catch (IOException e) {
-            log.info("Exception : {} ", e.getMessage() );
+            log.error("IO Exception while uploading : {} ", e.getMessage());
+            throw new RuntimeException("Error while uploading video, Exception : " + e.getMessage());
         }
-        return null;
     }
 
-    public ResponseEntity<String> downloadVideo(String videoCategory, MultipartFile file){
-        String path = null;
+    /**
+     * Downloads a video file identified by the given videoId.
+     *
+     * @param videoId The unique identifier of the video to be downloaded.
+     * @return ResponseEntity containing the video file as an InputStreamResource if
+     *         found,
+     *         otherwise returns ResponseEntity with status NOT_FOUND.
+     * @throws RuntimeException if an error occurs during the download process.
+     */
+    public ResponseEntity<InputStreamResource> downloadVideo(UUID videoId) {
         try {
-            if (videoCategory.equals("license")) {
-                path = videoPath + "license/" + file.getOriginalFilename();
-            } else{
-                return new ResponseEntity<>("Please select correct file Type", HttpStatus.BAD_REQUEST);
+
+            // Get Video
+            Videos video = videoRepoService.findById(videoId);
+
+            // No Video Found
+            if (video == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             }
-            file.transferTo(new File(path));
-//            Files.write(path, bytes);
-            return new ResponseEntity<>("File uploaded successfully", HttpStatus.OK);
-        } catch (IOException e) {
-//            e.printStackTrace();
-            log.info("Exception : {} ", e.getMessage() );
-            return new ResponseEntity<>("Failed to upload file", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            // Get File
+            File file = new File(video.getPath());
+
+            // No File Found
+            if (!file.exists()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            // Open Stream
+            MediaType mediaType = Utils.determineMediaType(video.getPath());
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+            return ResponseEntity.ok()
+                    .contentType(mediaType)
+                    .contentLength(file.length())
+                    .body(resource);
+
+        } catch (Exception e) {
+            log.error("IO Exception while downloading : {} ", e.getMessage());
+            throw new RuntimeException("Error while downloading video, Exception : " + e.getMessage());
         }
     }
 
-    private void createUploadDirectoryIfNotExists(String uploadDirPAth) throws IOException {
-        Path path = Paths.get(uploadDirPAth);
-        if (!Files.exists(path)) {
-            Files.createDirectories(path);
-        }
-    }
+    /**
+     * Deletes a video file identified by the given videoId.
+     *
+     * @param videoId The unique identifier of the video to be deleted.
+     * @throws RuntimeException if an error occurs during the deletion process.
+     */
+    public void deleteVideoById(UUID videoId) {
 
-    private String addSuffixToFileName(String fileName) {
-        int dotIndex = fileName.lastIndexOf('.');
-        if (dotIndex == -1) {
-            // No extension
-            return fileName + "_1";
-        } else {
-            // With extension
-            String name = fileName.substring(0, dotIndex);
-            String extension = fileName.substring(dotIndex);
-            return name + "_1" + extension;
+        // Get Video
+        Videos video = videoRepoService.findById(videoId);
+
+        // Delete Video
+        if (video != null) {
+            File file = new File(video.getPath());
+            if (file.exists()) {
+                file.delete();
+            }
+
+            // TODO : Handle case where file does not exist but data is present in DB
+            videoRepoService.deleteById(videoId);
+        }
+
+        // Log
+        else {
+            log.warn("Video not found with id : {} ", videoId);
+            // TODO : Throw Exception?
         }
     }
 
